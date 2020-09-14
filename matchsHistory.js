80d77f4bd3112d41bf73c30800c9ee2ba128b96f
Maycon Miranda <mayconjr_@live.com>
2020-09-14 12:07:43 -0300

    Fixing typos

--- matchsHistory.js
+++ matchsHistory.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        TM League Match History
 // @version     0.5
-// @description Adds last match results and link to the matchs in the league table.
+// @description Add last match results and link to the matchs in the league table.
 // @author      Maycon Miranda - https://github.com/mayc0njr
 // @downloadURL https://github.com/mayc0njr/trophyManager/blob/master/matchsHistory.js
 // @namespace   https://trophymanager.com
